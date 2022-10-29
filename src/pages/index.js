5a75c21d1206b127bb29b1c16c4b43a53358c923
Kuijpers Michael (s127608) <michael.kuijpers@student.ap.be>
2022-10-29 20:39:41 +0200

    Gatbsy Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 // Stap 1: Importeer React
 import * as React from 'react'
 import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 import Layout from '../components/layout'
 // Stap 2: definieer je component
 const IndexPage = () => {
@@ -8,6 +9,14 @@ const IndexPage = () => {
     <main>
       <Layout pageTitle="Welcome to Inghelbrecht Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="randomized unsplash image!"
+        src="https://source.unsplash.com/random/800x600"
+      />
+       <StaticImage
+        alt="Een Gatsby astronaut"
+        src="../images/040.png"
+      />
       </Layout>
       {/* <title>Home Page</title>
       <h1>Welcome to Inghelbrecht Agency!</h1>
@@ -18,5 +27,4 @@ const IndexPage = () => {
 }
 
 // Stap 3: Exporteer je component
-
 export default IndexPage
