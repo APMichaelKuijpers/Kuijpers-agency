d2c58d9d630a1b76258217d1b727f2729674633e
Kuijpers Michael (s127608) <michael.kuijpers@student.ap.be>
2022-10-29 20:27:17 +0200

    Finished React in Gatsby

---
+++ src/components/greeting.js
@@ -0,0 +1,10 @@
+// Defineren van de <Greeting> component
+import * as React from "react"
+
+const Greeting = (props) => {
+    return (
+      <p>Hi {props.name}!</p>
+    )
+  }
+
+export default Greeting
